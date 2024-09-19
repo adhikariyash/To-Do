79450bca4230b99f2a28c654c105c9ed3cc94229
ITZBaburam <139737597+Baburam99@users.noreply.github.com>
2024-09-19 23:57:18 +0545

    vercell

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ import { TaskModel } from './src/models/user.models.js';
 dotenv.config(); 
 
 const app = express();
-app.use(cors({ origin: 'https://to-do-frontend-sigma.vercel.app' }));
+app.use(cors());
 app.use(express.json());
 
 // db connect
