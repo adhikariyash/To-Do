f42bc578c2b98c8c2d806caecaa01cf894ad4556
ITZBaburam <139737597+Baburam99@users.noreply.github.com>
2024-09-20 00:19:33 +0545

    testing three

--- backend/index.js
+++ backend/index.js
@@ -8,8 +8,7 @@ import { TaskModel } from './src/models/user.models.js';
 dotenv.config(); 
 const app = express();
 app.use(cors({
-  origin: [`https://to-do-frontend-2brmma5vl-yashs-projects-a28e16d2.vercel.app
-`],
+  origin: ['http://localhost:5173'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type']
 }));
