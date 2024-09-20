619b8a6e4a46adcd3d6f034d47b72384459e6077
ITZBaburam <139737597+Baburam99@users.noreply.github.com>
2024-09-20 10:53:09 +0545

    back to old code

--- frontend/src/components/Todo.jsx
+++ frontend/src/components/Todo.jsx
@@ -1,31 +1,49 @@
-import React, { useState, useEffect } from "react";
-import Todo from './Todo';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-function Home() {
-  const [Todos, setTodos] = useState([]);
+function Todo() {
+  const [task, setTask] = useState();
+  
+ 
 
-  useEffect(() => {
-    axios.get("https://to-do-nine-khaki.vercel.app/tasks/get", {
-      headers: {
+  function handleTask() {
+    axios.post("https://to-do-nine-khaki.vercel.app/api/tasks", { task: task },{
+        headers: {
         'Content-Type': 'application/json',
-      }
-    }).then(result => setTodos(result.data))
-      .catch(err => console.log(err));
-  }, []);
-
-  const handleNewTask = (newTask) => {
-    setTodos(prevTodos => [...prevTodos, newTask]);
+    }
+    })
+      .then((result) => {
+        location.reload()
+      })
+      .catch((err) => console.log(err));
   }
 
   return (
-    <div className="h-screen bg-blue-950 flex flex-col items-center">
-      <div className="mb-10 w-full">
-        <Todo onTaskAdded={handleNewTask} />
+    <div className=" flex justify-center bg-blue-950 items-center w-full  flex-col">
+      <div className=" pt-10 w-1/2 flex flex-row justify-center">
+        <form  className="w-full justify-center items-center flex">
+          <input
+          type="text" required
+          placeholder="Task"
+            value={task}
+            onChange={(e) => {
+              setTask(e.target.value);
+            }}
+            className="border-2 text-lg sm:w-full px-7  font-bold lg:text-2xl p-2 rounded-xl border-black 
+             rounded-r-none"
+          />
+          <button
+            type="button"
+            onClick={handleTask}
+            className=" bg-black py-3 px-4 border-2 border-red-500 font-bold text-2xl  rounded-r-full text-white "
+          >
+            Add
+          </button>
+        </form>
       </div>
-    
+     
     </div>
   );
 }
 
-export default Home;
+export default Todo;
