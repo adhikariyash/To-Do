b3e001509f042c6bc2849b55583b31db5432dc84
ITZBaburam <139737597+Baburam99@users.noreply.github.com>
2024-09-21 12:54:11 +0545

    links update

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 8000
 
 
 app.use(cors({
-  origin:'https://to-1ttal3koa-yashs-projects-a28e16d2.vercel.app'
+  origin:'https://to-do-git-main-yashs-projects-a28e16d2.vercel.app'
 }));
 
 app.use(express.json());
